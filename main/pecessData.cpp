c51875d753a29f8d7d5d92a288019c700f094544
nvmotou <1617738321@qq.com>
2017-04-16 15:56:15 +0800

    生成线索网址或获取Symbol

--- main/pecessData.cpp
+++ main/pecessData.cpp
@@ -6,19 +6,27 @@ int main()
 {
 	ifstream r;
 	ofstream w;
-	string info[7];
-	r.open("data1.txt");
+	ofstream w1;
+	string info[3];
+	r.open("data3.txt");
 	w.open("data2.txt");
+	w1.open("data1x.txt",ios::app);
+	int num = 0;
 	while(!r.eof())
 	{
-		for(int i=0;i<7;i++)
+		for(int i=0;i<3;i++)
 		{
 			getline(r,info[i]);
 		}
-		if(info[3]!="") w<<("https://plants.usda.gov/"+info[3])<<endl;
-		if(info[4]!="") w<<("https://plants.usda.gov/"+info[4])<<endl;
+		w1<<info[0]<<endl;
+		num++;
+//		if(info[3]!="") w<<("https://plants.usda.gov/"+info[3])<<endl;
+//		if(info[4]!="") w<<("https://plants.usda.gov/"+info[4])<<endl;
 	}
+	cout<<num<<endl;
 	
+	w1.flush();
+	w1.close();
 	w.flush();
 	w.close();
 	r.close();
